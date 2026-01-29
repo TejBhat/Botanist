a23d53543a07aaef9fe5a77b6f51282fb9fef957
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-29 22:58:14 +0530

    feat: add home page

---
+++ app/(app)/Home.tsx
@@ -0,0 +1,18 @@
+import { View,Text,StyleSheet } from "react-native";
+
+export default function HomeScreen(){
+    return(
+        <View>
+             <View>
+            <Text>Homescreen</Text>
+        </View>
+        </View>
+       
+
+    );
+}
+
+const style=StyleSheet.create({
+
+
+})
