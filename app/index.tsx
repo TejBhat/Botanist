d10bf0bae1e2764990eaeb0ea128b78f6c1905df
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-29 23:00:20 +0530

    feat: add home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,7 @@
-import { Text, View } from "react-native";
+import { Redirect } from "expo-router";
+
+
 
 export default function Index() {
-  return (
-    <View>
-      <Text>Work in progress</Text>
-    </View>
-  );
+  return <Redirect href="/Home"/>
 }
