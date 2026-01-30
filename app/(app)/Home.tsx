db0c29950793db86fd9559a4aeb8a085937db091
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-30 23:09:54 +0530

    feat: add home page

--- app/(app)/Home.tsx
+++ app/(app)/Home.tsx
@@ -1,12 +1,20 @@
 import { View,Text,StyleSheet } from "react-native";
+import {Card} from "react-native-paper";
 
 export default function HomeScreen(){
     return(
-        <View style={style.container}>
-             <View>
-            <Text>Homescreen</Text>
-        </View>
-        </View>
+        
+             <View style={style.container}>
+            
+            <View>
+                <Card style={style.button}>
+                <Text>Scan Image</Text>
+                </Card>
+           
+       </View>
+       </View>
+        
+       
        
 
     );
@@ -16,6 +24,10 @@ const style=StyleSheet.create({
 container:{
     flex:1,
     backgroundColor:"green",
-}
+},
+button:{
+
+},
+
 
 });
