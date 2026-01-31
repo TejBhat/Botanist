7ac470de80799a203183dab0a80fe6850434ab56
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-31 20:33:09 +0530

    feat: add about botanist page

---
+++ app/(app)/about.tsx
@@ -0,0 +1,10 @@
+import { View , Text} from "react-native";
+
+ export default function About(){
+    return(
+        <View>
+            <Text>Welcome test About page</Text>
+        </View>
+
+    );
+ }
