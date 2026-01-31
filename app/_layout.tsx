2a07193266dcd63a803d5a3d41060a9eb86d7a2e
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-31 20:34:45 +0530

    feat: add layout navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,10 @@
 import { Stack } from "expo-router";
+import { PaperProvider } from "react-native-paper";
 
 export default function RootLayout() {
   return (
-  <Stack screenOptions={{headerShown:false}}>
-     <Stack.Screen name="/index"/>
-     <Stack.Screen name="/Home"/>
-  </Stack>
+    <PaperProvider>
+      <Stack screenOptions={{headerShown:false}}></Stack>
+    </PaperProvider>
   );
 }
