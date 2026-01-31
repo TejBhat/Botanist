7827269f9111826f232f544d9441499e896a3320
Tej M Bhat <tejbhat2004@gmail.com>
2026-01-31 16:17:30 +0530

    feat: add home page

--- app/(app)/Home.tsx
+++ app/(app)/Home.tsx
@@ -1,32 +1,46 @@
 import { View,Text,StyleSheet } from "react-native";
-import {Card} from "react-native-paper";
+import {Button} from "react-native-paper";
 
 export default function HomeScreen(){
     return(
         
-             <View style={style.container}>
-            
-            <View>
-                <Card style={style.button}>
-                <Text>Scan Image</Text>
-                </Card>
-           
-       </View>
-       </View>
-        
-       
-       
-
+            <View style={style.wholescreen}>
+                <Text style={style.title}>Botanist</Text>
+                <Button mode="contained" style={style.button} 
+                contentStyle={style.buttontext} 
+                labelStyle={style.buttonLabel}>Take a photo</Button> {/*click the button, it goes to camera to take the photo*/}
+           </View>
     );
 }
 
 const style=StyleSheet.create({
-container:{
+    title:{
+        position:"absolute",
+        top:65,
+        fontWeight:"bold",
+        fontSize:40,
+        color:"#ffff00",
+    },
+wholescreen:{
     flex:1,
-    backgroundColor:"green",
+    backgroundColor:"#006400",
+    justifyContent:"center",
+    alignItems:'center',
+
 },
 button:{
-
+  borderRadius:30,
+  width:250,  
+  backgroundColor:"#ffff00",
+},
+buttontext:{
+    height:60,
+    justifyContent:"center",
+},
+buttonLabel:{
+      fontWeight:"bold",
+      fontSize:20,
+      color:"#006400",
 },
 
 
