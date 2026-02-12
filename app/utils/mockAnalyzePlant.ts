8ddb5678c0b216690eb95ce796c117292b4e8b4f
Tej M Bhat <tejbhat2004@gmail.com>
2026-02-12 20:37:54 +0530

    feat: add mockanalyze plants

--- app/utils/mockAnalyzePlant.ts
+++ app/utils/mockAnalyzePlant.ts
@@ -1,6 +1,5 @@
 import { PLANTS } from "../../data/plants";
 
-// ISSUE FIX: Added proper type definition for return value
 export type AnalysisResult = 
   | { isPlant: false }
   | { isPlant: true; plantId: string; confidence: number };
@@ -14,7 +13,6 @@ export function mockAnalyzePlant(imageUri: string): AnalysisResult {
     };
   }
 
-  // ISSUE FIX: Changed 'plants' to 'Object.keys(PLANTS)' since PLANTS is an object
   const plantKeys = Object.keys(PLANTS);
   const randomPlant = plantKeys[Math.floor(Math.random() * plantKeys.length)];
 
